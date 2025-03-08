b44d9560d7b0c41c0c233368384650c53335a150
Muhammad Zain Zameer <137919776+Zain-Zameer@users.noreply.github.com>
2025-03-08 19:41:41 +0500

    Added day 01

--- Day 01/tests/day01_tests.py
+++ Day 01/tests/day01_tests.py
@@ -1,9 +1,12 @@
 # you don't need to take a look at it newbie
 import subprocess
+import json 
+
+points = 0
 
 def run_user_code():
     try:
-        result = subprocess.run(["python", "main.py"], capture_output=True, text=True)
+        result = subprocess.run(["python", "../day01_tasks.py"], capture_output=True, text=True)
         return result.stdout.strip().split("\n") 
     except Exception as e:
         print("⚠️ Error running main.py:", e)
@@ -15,19 +18,31 @@ def test_college_name(output):
             output[0] = int(output[0])
             print("- Input college name must be a string. for example: 'Stanford'")
         except:
-            print("Passed task 1")
+            print("- Passed task 1")
+            global points
+            points+=1
 def test_age(output):
     if(int(output[1])):
-        print("- Passed Task 2.")
+        global points
+        points+=1
+        print("- Passed Task 2")
 
 def test_reason(output):
     if(len(output[2]) < 5):
         print("- Enter a valid reason of learning python.")
     else:
+        global points
+        points+=1
         print("- Passed Task 3")
 
 output = run_user_code()
-print(output)
 test_college_name(output)
 test_age(output)
 test_reason(output)
+if(points==3):
+    with open("../progress.json") as f:
+        d = json.load(f)
+    d["Day 01"] = "Completed"
+    d["Points"] = points
+    with open("../progress.json","w") as f:
+        json.dump(d,f,indent=4)
